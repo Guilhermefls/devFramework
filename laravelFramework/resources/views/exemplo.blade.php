eebcfe7a193093ed1ad70aea7948559b73d8a937
Guilhermefls <larreasalvatico@gmail.com>
2024-02-21 16:07:01 -0400

    codigo certo

---
+++ laravelFramework/resources/views/exemplo.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>exemplo</title>
+    </head>
+    <body>
+        <h2>Guilherme</h2>
+        <p>ola Guilherme</p>
+    </body>
+</html>
--- laravelFramework/routes/web.php
+++ laravelFramework/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\ExemploController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/exemplo', function () {
+//    return view('exemplo');
+//});
+
+Route::get('/exemplo','app\Http\Controllers\ExemploController@index');
